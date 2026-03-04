da9c6919282c0d4064322fc57376f015965a5c36
Aiyan Chowdhury <aiyan041c10@outlook.com.au>
2026-03-03 21:48:41 -0500

    removed comments, fixed formatting, and fixed vercel deployment issue (hopefully) 	modified:   public/index.html 	modified:   public/notes.html 	modified:   server/index.js

--- server/index.js
+++ server/index.js
@@ -11,32 +11,34 @@ const assetsDir = path.join(rootDir, "assets");
 const nodeModulesDir = path.join(rootDir, "node_modules");
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(publicDir));
+app.use("/assets", express.static(assetsDir));
+app.use("/vendor", express.static(nodeModulesDir));
+
+app.use(
+  "/vendor/@fortawesome",
+  express.static(path.join(nodeModulesDir, "@fortawesome")),
+);
+app.use("/vendor/gsap", express.static(path.join(nodeModulesDir, "gsap")));
+app.use("/vendor/lenis", express.static(path.join(nodeModulesDir, "lenis")));
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(publicDir, "notes.html"));
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(publicDir, "index.html"));
+});
+
+app.use((req, res, next) => {
+  const ext = path.extname(req.path);
+  if (ext && ext !== ".html") {
+    return next();
+  }
+  res.status(404).sendFile(path.join(publicDir, "404.html"));
+});
+
 if (process.env.NODE_ENV !== "production") {
-  app.use(express.static(publicDir));
-
-  app.use("/assets", express.static(assetsDir));
-
-  app.use("/vendor", express.static(nodeModulesDir));
-
-  app.use(
-    "/vendor/@fortawesome",
-    express.static(path.join(nodeModulesDir, "@fortawesome")),
-  );
-  app.use("/vendor/gsap", express.static(path.join(nodeModulesDir, "gsap")));
-  app.use("/vendor/lenis", express.static(path.join(nodeModulesDir, "lenis")));
-
-  app.get("/", (req, res) => {
-    res.sendFile(path.join(publicDir, "index.html"));
-  });
-
-  app.get("/notes", (req, res) => {
-    res.sendFile(path.join(publicDir, "notes.html"));
-  });
-
-  app.use((req, res) => {
-    res.status(404).sendFile(path.join(publicDir, "404.html"));
-  });
-
   app.listen(PORT, () => {
     console.log(`visit project canopy's website at http://localhost:${PORT}`);
   });
